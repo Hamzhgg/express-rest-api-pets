4c656f6a5aa6c1c4dbba6f5b358c0b42a9232fea
Hamzhgg <gharibehhamzh@gmail.com>
2024-11-10 11:25:49 +0300

    all

---
+++ server.js
@@ -0,0 +1,24 @@
+const dotenv = require('dotenv')
+dotenv.config()
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+// Import the controller file
+const petRouter = require('./controllers/pets')
+
+// Mongoose Connection
+mongoose.connect(process.env.MONGODB_URI)
+
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}.`)
+})
+
+app.use(express.json())
+
+// ROUTERS
+app.use('/pets', petRouter)
+
+app.listen(3000, () => {
+    console.log('The express app is ready!')
+})
